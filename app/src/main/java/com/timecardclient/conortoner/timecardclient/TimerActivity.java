a22a9bc319a34e00ca25bb6a1a291a31c0cb92e7
Alec Tunbridge <alec.tunbridge@gmail.com>
2018-12-01 15:43:38 +0000

    Updated url for posting times to server and added dodgy permissions hack

--- app/src/main/java/com/timecardclient/conortoner/timecardclient/TimerActivity.java
+++ app/src/main/java/com/timecardclient/conortoner/timecardclient/TimerActivity.java
@@ -5,12 +5,15 @@ import android.app.AlertDialog;
 import android.app.MediaRouteButton;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -74,6 +77,23 @@ public class TimerActivity extends AppCompatActivity {
     private Button resetButton = null;
 
 
+    private static final int REQUEST_CODE = 0x11;
+    private String[] permissions = {"android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE", "android.permission.INTERNET"};
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // save file
+            } else {
+                Toast.makeText(getApplicationContext(), "PERMISSION_DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     private Runnable reQue = new Runnable() {
         @Override
         public void run() {
@@ -93,6 +113,7 @@ public class TimerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE); // without sdk version check
         setContentView(R.layout.activity_timer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -336,7 +357,7 @@ public class TimerActivity extends AppCompatActivity {
         }
 
         private int testHttpPost() throws IOException {
-            URL url = new URL(host + "/result");
+            URL url = new URL(host + "/raw-time");
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("POST");
             urlConnection.setRequestProperty("Content-Type", "application/json");
